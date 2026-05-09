1c1cce5c39712d0d4ca5c1749103595d177d69e1
flavio-simonelli <flavio.simonelli.cd@gmail.com>
2026-05-09 19:57:25 +0200

    feat: add getter methods for all fields in RawFlight class

--- spark/src/main/java/it/uniroma2/sae/model/RawFlight.java
+++ spark/src/main/java/it/uniroma2/sae/model/RawFlight.java
@@ -34,4 +34,32 @@ public class RawFlight implements Serializable {
     private Double securityDelay;
     private Double lateAircraftDelay;
 
+        public Integer getYear() { return year; }
+    public Integer getMonth() { return month; }
+    public Integer getDayOfMonth() { return dayOfMonth; }
+    public String getOpUniqueCarrier() { return opUniqueCarrier; }
+    public String getOpCarrierFlNum() { return opCarrierFlNum; }
+    public Long getOriginAirportId() { return originAirportId; }
+    public Long getOriginCityMarketId() { return originCityMarketId; }
+    public String getOriginStateAbr() { return originStateAbr; }
+    public Long getDestAirportId() { return destAirportId; }
+    public Long getDestCityMarketId() { return destCityMarketId; }
+    public String getDestStateAbr() { return destStateAbr; }
+    public Integer getCrsDepTime() { return crsDepTime; }
+    public Integer getDepTime() { return depTime; }
+    public Double getDepDelay() { return depDelay; }
+    public Integer getCrsArrTime() { return crsArrTime; }
+    public Integer getArrTime() { return arrTime; }
+    public Double getArrDelay() { return arrDelay; }
+    public Boolean getCancelled() { return cancelled; }
+    public String getCancellationCode() { return cancellationCode; }
+    public Boolean getDiverted() { return diverted; }
+    public Double getActualElapsedTime() { return actualElapsedTime; }
+    public Double getDistance() { return distance; }
+    public Double getCarrierDelay() { return carrierDelay; }
+    public Double getWeatherDelay() { return weatherDelay; }
+    public Double getNasDelay() { return nasDelay; }
+    public Double getSecurityDelay() { return securityDelay; }
+    public Double getLateAircraftDelay() { return lateAircraftDelay; }
+
 }
